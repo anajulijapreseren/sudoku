dd2f3388a60d4717fd195622f63f3d52e2092589
Ana Julija Preseren <ana.julija@preseren.me>
2020-08-27 20:15:07 +0200

    started thinking which methods do  need to work with cookies

--- Model/cookies.py
+++ Model/cookies.py
@@ -7,3 +7,13 @@ def get_random_cookie():
     result_str = ''.join(random.choice(letters) for i in range(10))
     # print("Random string of length", 10, "is:", result_str)
     return result_str
+
+def create_cookie_in_dictionary(cookie):
+
+def update_quiz_in_dictionary(cookie, quiz):
+
+def update_solution_to_dictionary(cookie, solution)
+
+def read_quiz_from_dictionary(cookie):
+
+def read_solution_from_dictionary(cookie):
